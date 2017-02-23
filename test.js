5ca9312ad03b5854bc272c8211c2b51ac2abb649
Ben Drucker <bvdrucker@gmail.com>
2017-02-23 15:06:44 -0800

    Add tests for updating a stack

--- test.js
+++ test.js
@@ -15,6 +15,11 @@ var template = JSON.stringify({
     BucketPrefix: {
       Description: 'Prefix for bucket',
       Type: 'String'
+    },
+    BucketSuffix: {
+      Description: 'Suffix for bucket',
+      Type: 'String',
+      Default: 'bucket'
     }
   },
   Resources: {
@@ -22,7 +27,7 @@ var template = JSON.stringify({
       Type: 'AWS::S3::Bucket',
       Properties: {
         BucketName: {
-          'Fn::Sub': '${BucketPrefix}-${AWS::StackName}'
+          'Fn::Sub': '${BucketPrefix}-${AWS::StackName}-${BucketSuffix}'
         }
       }
     }
@@ -76,7 +81,75 @@ test('create', function (t) {
       t.equal(stacks.length, 1)
       var stack = stacks[0]
       t.equal(stack.Outputs.length, 1)
-      t.equal(stack.Outputs[0].OutputValue, 'test-' + name)
+      t.equal(stack.Outputs[0].OutputValue, 'test-' + name + '-bucket')
+      callback()
+    })
+  }
+
+  function destroy (callback) {
+    cfn.deleteStack({StackName: name}, callback)
+  }
+})
+
+test.only('update', function (t) {
+  var name = 'cfn-stack-' + cuid()
+
+  series([
+    create,
+    awaitCreate,
+    update,
+    awaitUpdate,
+    validate,
+    destroy
+  ], t.end)
+
+  function create (callback) {
+    var options = {region: 'us-east-1'}
+
+    cfnStack({
+      Name: name,
+      Template: template,
+      Parameters: {
+        BucketPrefix: 'test'
+      }
+    }, options, callback)
+  }
+
+  function awaitCreate (callback) {
+    cfn.waitFor('stackCreateComplete', {StackName: name}, callback)
+  }
+
+  function update (callback) {
+    var options = {region: 'us-east-1'}
+
+    cfnStack({
+      Name: name,
+      Template: template,
+      Parameters: {
+        BucketPrefix: 'test',
+        BucketSuffix: 'newfix'
+      }
+    }, options, callback)
+  }
+
+  function awaitUpdate (callback) {
+    cfn.waitFor('stackUpdateComplete', {StackName: name}, callback)
+  }
+
+  function validate (callback) {
+    var params = {
+      StackName: name
+    }
+
+    cfn.describeStacks(params, function (err, data) {
+      if (err) return callback(err)
+
+      var stacks = data.Stacks
+
+      t.equal(stacks.length, 1)
+      var stack = stacks[0]
+      t.equal(stack.Outputs.length, 1)
+      t.equal(stack.Outputs[0].OutputValue, 'test-' + name + '-newfix')
       callback()
     })
   }
